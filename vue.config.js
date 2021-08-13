7cfa939296959e7308133fe12acbf165d6d04a02
phann-phearoen <74291680+phann-phearoen@users.noreply.github.com>
2021-08-13 08:47:56 +0700

    resolved issue #45

--- vue.config.js
+++ vue.config.js
@@ -5,6 +5,12 @@ module.exports = {
       quasar: {
         importStrategy: 'kebab',
         rtlSupport: true
+      },
+      i18n: {
+        locale: 'ja',
+        fallbackLocale: 'en',
+        localeDir: 'assets/locales',
+        enableInSFC: true
       }
     },
 
